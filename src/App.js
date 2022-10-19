189939ead374659c4ca4bb61f4a556dc94db197b
Haider Khan <115626636+Haiderkhan10@users.noreply.github.com>
2022-10-19 12:59:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Calender from './components/Calender';
+function App() {
+  return (
+    <div className="App">
+      <div className='nav'>
+        <h1>Internship Project</h1>
+        <Calender />
+      </div>
+    </div>
+  );
+}
+
+export default App;
